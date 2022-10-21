f3b3f6590e14c5d84ddc2ac2b04bb73beda8b601
FermiParadox12 <114105134+FermiParadox12@users.noreply.github.com>
2022-10-20 17:29:52 -0700

    Added audio resources, restructured assets folder

--- interface/Pause.gd
+++ interface/Pause.gd
@@ -1,14 +1,21 @@
 extends Control
 
-
+export(PackedScene) var menu
 
 func _ready():
-	visible = false
-	
+	pass
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		var pause_state = not get_tree().paused
 		get_tree().paused = pause_state
-		visible = pause_state
+		get_node("..").visible = pause_state
 	
+
+
+func _on_Game_Quit_button_up():
+	get_tree().quit()
+
+
+func _on_Menu_Quit_button_up():
+	pass
---
+++ interface/Title Screen.gd
@@ -0,0 +1,38 @@
+extends Control
+
+const FADE = 1.0
+
+onready var timer = $Timer
+export(PackedScene) var Start_Scene
+
+func _ready():
+	modulate = Color(0, 0, 0, 1) #Darken Screen
+	fade_in(true)
+	
+
+func _on_Start_button_up():
+	timer.set_wait_time(FADE)
+	fade_out()
+	timer.start()
+	
+
+func _on_Quit_button_up():
+	get_tree().quit()
+	
+
+func _on_Timer_timeout():
+	get_tree().change_scene_to(Start_Scene)
+	
+
+func fade_in(i):
+	var tween = get_node(".").create_tween() #create tween
+	if i == true: #first bootup
+		tween.tween_property(self, "modulate", Color(1, 1, 1, 1), 3.0).from_current()
+	else:
+		tween.tween_property(self, "modulate", Color(1, 1, 1, 1), FADE).from_current()
+	
+
+func fade_out():
+	var tween = get_node(".").create_tween() #create tween
+	tween.tween_property(self, "modulate", Color(0, 0, 0, 1), FADE).from_current()
+	
