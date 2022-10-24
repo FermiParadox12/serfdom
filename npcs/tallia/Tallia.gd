954114385c0199f6e44093e859a1d699843859e3
FermiParadox12 <114105134+FermiParadox12@users.noreply.github.com>
2022-10-24 16:43:56 -0700

    Started dialogue script for Tallia, proximity detection for Tallia,

---
+++ npcs/tallia/Tallia.gd
@@ -0,0 +1,58 @@
+extends Area2D
+
+enum{
+	SAFEROOM
+	STARTED
+	FAILED
+	COMPLETED
+}
+
+var lines_spoken = 0
+var quest_progress
+var in_range = false
+
+
+func _ready():
+	$Popup.scale = Vector2.ZERO
+
+func _input(event):
+	if event.is_action_pressed("interact") && in_range == true:
+		speak(quest_progress, lines_spoken)
+
+
+func speak(prog,lines):
+	var r = null
+	match prog:
+		SAFEROOM: #First meeting Tallia
+			match lines:
+				0:
+					return("EEEEK! PLease don't hurt me!!")
+				1:
+					return("...")
+				2:
+					return("Wait, you're not a guard!")
+				3:
+					return("Sorry about that... Who might you be?")
+				_:
+					return(r)
+				
+			
+		STARTED: #Tallia asks player to find mother
+			pass
+		FAILED: #Player says no
+			pass
+		COMPLETED: #Player finds mother & returns to Tallia
+			pass
+
+
+func _on_Tallia_body_entered(body):
+	if body.name == "Player":
+		in_range = true
+		var tween = get_node(".").create_tween() #create tween for popup's scale from 0->1 and ease in/out
+		tween.tween_property($Popup, "scale", Vector2(1, 1), 0.5).from_current().set_ease(Tween.EASE_IN_OUT)
+
+func _on_Tallia_body_exited(body):
+	if body.name == "Player":
+		in_range = false
+		var tween = get_node(".").create_tween() #create tween for popup's scale from 1->0 and ease in/out
+		tween.tween_property($Popup, "scale", Vector2.ZERO, 0.5).from_current().set_ease(Tween.EASE_IN_OUT)
